e8364f84b8264f1b8438d7c24430df941397e52e
Shashwat Shivam <ankurshaswat@yahoo.co.in>
2018-03-05 12:51:16 +0530

    Modified main page

--- Project1/Code/src/program.cpp
+++ Project1/Code/src/program.cpp
@@ -1,15 +1,21 @@
-// Program.h
-#include <iostream>
-// #include "module1/Fig3D.h"
-// #include "module1/Vertice.h"
-// #include "module1/Edge.h"
-
-using namespace std;
+/** @file program.cpp
+@author Lastname:Firstname:A00123456:cscxxxxx
+@version Revision 1.0
+@brief Main file documentation.
+@details Main File
+@date Monday, March 5, 2018
+*/
 
 /**
-they
-**/
+@mainpage
+This is the main file of the program. This will be used to launch the interface to select files and other things. Refer to class list
+ for detailed description of other classes.
+*/
 
+
+
+#include <iostream>
+using namespace std;
 int main()
 {
 
