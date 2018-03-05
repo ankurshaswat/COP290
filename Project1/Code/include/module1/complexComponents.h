8f54af6190fdfc35331c00dfca47d1a061c2e612
Sushant101 <rathisushant5@gmail.com>
2018-03-05 13:26:51 +0530

    Modified headers

---
+++ Project1/Code/include/module1/complexComponents.h
@@ -0,0 +1,24 @@
+// Some high level components used as intermediates in our algorithms
+#ifndef COMPLEXCOMPONENTS_H
+#define COMPLEXCOMPONENTS_H
+
+#include "basicComponents.h"
+
+class EdgeLoop: public Face //a planar closed loop of edges (connected end-to-end), basically a face extended with other methods
+{
+public:
+bool isContained(EdgeLoop a); // to check whether an edgeLoop is completely contained in another 
+};
+class WireFrame
+{
+
+private:
+Edge edgeList[100];
+
+public:
+Edge* getEdgeList();
+ 
+};
+
+
+#endif
